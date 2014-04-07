3d562186c77ab4deb9af9796203c97848ee03bb5
karneds <karneds@gmail.com>
2014-04-07 16:15:08 +0400

    fix missing redactor.min.css

--- widgets/RedactorAsset.php
+++ widgets/RedactorAsset.php
@@ -21,13 +21,11 @@ class RedactorAsset extends \yii\web\AssetBundle
     public function init()
     {
 
-        if (YII_DEBUG) {
+        if (YII_DEBUG)
             $this->js[] = 'redactor.js';
-            $this->css[] = 'redactor.css';
-        } else {
+        else
             $this->js[] = 'redactor.min.js';
-            $this->css[] = 'redactor.min.css';
-        }
+        $this->css[] = 'redactor.css';
     }
 
 }
